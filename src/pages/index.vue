ff92108c4869d746b2891095415aaede6cfe524e
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2026-03-10 20:48:27 +0100

    feat: wire up components on home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import SandwichGenerator from '@/components/SandwichGenerator.vue'
+import SandwichList from '@/components/SandwichList.vue'
+</script>
+
 <template>
-  <h1>Happy Coding</h1>
+  <main>
+    <h1>🥖 Générateur de Sandwich</h1>
+    <SandwichGenerator />
+    <hr />
+    <SandwichList />
+  </main>
 </template>
